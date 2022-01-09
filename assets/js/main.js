0fc57c22cca81b6f169d3f89d4f13cdb0fe8bbe4
Tiago Silva <tiagobsb84@gmail.com>
2022-01-08 21:22:43 -0300

    Projeto finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
 function buscarCep(){
+    $(".carregar").show();
+
     let cep = document.getElementById("cep").value;
     let url = `http://viacep.com.br/ws/${cep}/json/`;
-  
+    
     $.ajax({
         url: url,
         type: "GET",
         success: function(response){
+            $(".titulo_cep").html(`CEP: ${response.cep}`);
             $("#cidade").html(response.localidade);
+            $("#estado").html(response.uf);
+            $("#bairro").html(response.bairro);
+            $("#endereco").html(response.logradouro);
+            $(".cep_ocultar").show();
+            $(".carregar").hide();
         }
     })
-}
+};
+
+$(function(){
+    $(".cep_ocultar").hide();
+    $(".carregar").hide();
+})
